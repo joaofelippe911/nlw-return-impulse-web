437c6edc6cfd9346c1f8271f69d388c3f3d7b643
João Felippe <joaofelippe0811@hotmail.com>
2022-05-16 11:03:44 -0300

    added light theme

--- src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -22,7 +22,7 @@ export function FeedbackSuccessStep({ onFeedbackRestartRequested }: FeedbackSucc
           <button
             type="button"
             onClick={onFeedbackRestartRequested}
-            className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500 focus:ring-offset-zinc-900"
+            className="py-2 px-6 mt-6 bg-zinc-100 dark:bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-200 dark:hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500 focus:ring-offset-white dark:focus:ring-offset-zinc-900"
           >
             Quero enviar outro
           </button>
